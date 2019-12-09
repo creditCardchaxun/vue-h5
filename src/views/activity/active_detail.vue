6f4026ad2c4fb5b5e34f2ee32b941762be912427
马明群_迈动互联 <mamingqun@bdideal.com>
2019-12-09 20:07:18 +0800

    12.间距过高的问题

--- src/views/activity/active_detail.vue
+++ src/views/activity/active_detail.vue
@@ -103,7 +103,7 @@ export default {
 <style scoped>
 .newsdetail{width:100%;margin:0 auto; min-height: 100%;padding-bottom:5.96rem;box-sizing: border-box;position:relative;}
 .newsdatails{width:90%;height:auto;margin:0.6rem auto 0;}
-.newsdatails h2{font-size:0.6rem;color:#2d2d2d;line-height: 0.87rem;max-height: 1.7rem;min-height:0.87rem;overflow: hidden;}
+.newsdatails h2{font-size:0.6rem;color:#2d2d2d;line-height: 0.87rem;max-height: 0.8rem;min-height:0.87rem;overflow: hidden;}
 .newsdatails .date{font-size:0.34rem;color:#aeaeae;margin:0.6rem 0;}
 .newsdatails .mains-new{font-size:0.36rem;color:#000;padding-bottom:1rem;}
 .newsdatails .mains-new img{width:100%;height:auto;margin:0 auto;}
