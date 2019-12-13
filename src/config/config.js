ad17cdb2327ed306997a79d391e7e21617ce1156
马明群_迈动互联 <mamingqun@bdideal.com>
2019-12-13 19:08:48 +0800

    去掉分享链接的s

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ var config
 let configTest = {
   BASEURL: 'http://shengxi.iyunfish.com' || process.env.BASE_URL,
   shareIcon: 'https://wanda-h5.bdideal.com/shengxi-test/icon/fx.png',
-  shareLink: 'https://wanda-h5.bdideal.com/shengxi-test/index.html',
+  shareLink: 'http://wanda-h5.bdideal.com/shengxi-test/index.html',
   shareTitle: 'baseLIVING精品居舍',
   shareContent:'为城市社群打造充满灵感的生活空间',
   sysType: 1
@@ -12,7 +12,7 @@ let configTest = {
 let configProduction = {
   BASEURL: 'http://shengxi.iyunfish.com' || process.env.BASE_URL,
   shareIcon: 'https://wanda-h5.bdideal.com/shengxi/icon/fx.png',
-  shareLink: 'https://wanda-h5.bdideal.com/shengxi/index.html',
+  shareLink: 'http://wanda-h5.bdideal.com/shengxi/index.html',
   shareTitle: 'baseLIVING精品居舍',
   shareContent:'为城市社群打造充满灵感的生活空间',
   sysType: 1
