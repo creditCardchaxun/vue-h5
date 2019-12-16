f6f589ee4fc0b08148056597d62cf2c7fb54faa0
马明群_迈动互联 <mamingqun@bdideal.com>
2019-12-16 10:20:31 +0800

    地图中英文样式 统一

--- src/components/map.vue
+++ src/components/map.vue
@@ -5,7 +5,8 @@
       <div v-show="fullScreen" :class="['button','window-button']" @click="changeFullScreen"></div>
       <div v-show="!fullScreen" :class="['button','full-button']" @click="openapp"></div>
       <aheaders v-if="!fullScreen" status="3" @toback="toreplace" :showNav="false" :showLan="true"></aheaders>
-      <div id="container" ref="mapbox" :class="[$store.state.lang==='en-US'? 'blackwhite':'']"></div>
+      <!-- <div id="container" ref="mapbox" :class="[$store.state.lang==='en-US'? 'blackwhite':'']"></div> -->
+      <div id="container" ref='mapbox'  class="blackwhite"></div>
       <div class="map-info" v-show="!fullScreen">
         <div id="firstdiv">
           <div class="firstdiv-wrap">
@@ -197,7 +198,7 @@ export default {
         center: _this.mapCenter, //初始化地图中心点 
         zoom: 10, //地图显示的缩放级别
         lang: lang,//zh_cn：中文简体，en：英文，zh_en：中英文对照
-        mapStyle: 'amap://styles/whitesmoke', //设置地图样式 远山黛.
+        // mapStyle: 'amap://styles/whitesmoke', //设置地图样式 远山黛.
         zoomEnable: !_this.fullScreen,
         dragEnable: !_this.fullScreen,
       });
@@ -726,6 +727,14 @@ export default {
 </script>
 
 <style lang="less">
+.amap-copyright {
+  display: none !important;
+}
+
+.amap-logo{
+  display: none !important;
+}
+
 .blackwhite .amap-layer {
   filter: grayscale();
 }
