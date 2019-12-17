fc0351300b533614c2ae6b0f146f7310e055f5aa
马明群_迈动互联 <mamingqun@bdideal.com>
2019-12-17 10:06:52 +0800

    无法受理此单逻辑处理

--- src/views/Booking-Management/booking-detail.vue
+++ src/views/Booking-Management/booking-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bookingdetail">
-    <aheaders status='3' @toback='toreplace' :showLan='true'></aheaders>
+    <aheaders status="3" @toback="toreplace" :showLan="true"></aheaders>
     <div class="detail">
       <ul>
         <li>
@@ -23,7 +23,7 @@
           <span class="detail-name">{{$t('m.feedback11')}}</span>
           <span>{{listInfo.intention}}</span>
         </li>
-        <li class='detail-li'>
+        <li class="detail-li">
           <span class="detail-name">{{$t('m.feedback12')}}</span>
           <span>{{listInfo.book_number}}</span>
         </li>
@@ -34,22 +34,28 @@
       </ul>
 
       <div class="detail-button">
-        <van-button plain hairline type="info" color="#5975a9" @click="showPopup">{{$t('m.feedback15')}}</van-button>
+        <van-button
+          plain
+          hairline
+          type="info"
+          color="#5975a9"
+          @click="showPopup"
+        >{{$t('m.feedback15')}}</van-button>
         <van-popup v-model="show">
           <div class="box">
             <!-- <input type="text" placeholder="请填写拒绝受理此单的原因" /> -->
-            <textarea cols="15" rows="5" v-model='desc' :placeholder="$t('m.feedback16')"></textarea>
-            <van-button class='box-btn' :disabled="isclick" @click="closePopup">{{$t('m.s4')}}</van-button>
-              <img src="../../assets/images/remove.png" alt="" class='img_remove'  @click="tohideModel">
-              <!-- <van-icon name="close" /> -->
+            <textarea cols="15" rows="5" v-model="desc" :placeholder="$t('m.feedback16')"></textarea>
+            <van-button class="box-btn" :disabled="isclick" @click="closePopup">{{$t('m.s4')}}</van-button>
+            <img src="../../assets/images/remove.png" alt class="img_remove" @click="tohideModel" />
+            <!-- <van-icon name="close" /> -->
           </div>
           <!-- <textarea cols="15" rows="5" placeholder="请填写拒绝受理此单的原因"></textarea> -->
         </van-popup>
         <van-button type="info" color="#5975a9" @click="showRight">{{$t('m.others11')}}</van-button>
         <van-popup v-model="show_right">
           <div class="good">
-            <img src="../../assets/images/checkmark.png" alt style='width:1.05rem;height:0.95rem;' />
-            <p style='color:#000;font-size:0.4rem;'>{{$t('m.others12')}}</p>
+            <img src="../../assets/images/checkmark.png" alt style="width:1.05rem;height:0.95rem;" />
+            <p style="color:#000;font-size:0.4rem;">{{$t('m.others12')}}</p>
           </div>
         </van-popup>
       </div>
@@ -68,7 +74,7 @@ import Vue from "vue";
 import aheaders from "@/components/Header";
 import footers from "@/components/Footer";
 import interfaces from "@/utils/api.js";
-import { Tab, Tabs, NavBar, Search,Toast,Icon } from "vant";
+import { Tab, Tabs, NavBar, Search, Toast, Icon } from "vant";
 
 Vue.use(Tab).use(Tabs);
 Vue.use(NavBar).use(NavBar);
@@ -82,85 +88,90 @@ export default {
     return {
       show: false,
       show_right: false,
-      ids:'',
-      desc:'',
-      listInfo:{},
-      getHeight:{
-        minHeight:''
+      ids: '',
+      desc: '',
+      listInfo: {},
+      getHeight: {
+        minHeight: ''
       },
-      disabled:false
+      disabled: false
     };
   },
   components: {
     aheaders,
     footers
   },
-     computed:{
-       isclick(){
-          if(!this.desc)return true
-          else return false;
-       },
-    //  this.getHeight.minHeight = (window.outerHeight/window.outerWidth * 10.8 - 5.96)+'rem'
-      //  minHeight(){
-      //   return (window.outerHeight/window.outerWidth * 10.8 - 5.96)+'rem'
-      // }
+  computed: {
+    isclick() {
+      if (!this.desc) return true
+      else return false;
     },
-  created(){
-    this.getHeight.minHeight = (window.outerHeight/window.outerWidth * 10.8 - 5.96)+'rem'
-    this.ids=this.$route.params.id
-    localStorage.setItem('bookId',this.ids)
+    //  this.getHeight.minHeight = (window.outerHeight/window.outerWidth * 10.8 - 5.96)+'rem'
+    //  minHeight(){
+    //   return (window.outerHeight/window.outerWidth * 10.8 - 5.96)+'rem'
+    // }
+  },
+  created() {
+    this.getHeight.minHeight = (window.outerHeight / window.outerWidth * 10.8 - 5.96) + 'rem'
+    this.ids = this.$route.params.id
+    localStorage.setItem('bookId', this.ids)
     this.getdetailbook(this.ids)
   },
-    mounted(){
-      $eventbus.$on("changeLang", (res)=>{
-         this.ids=this.$route.params.id
-        localStorage.setItem('bookId',this.ids)
-        this.getdetailbook(this.ids)
-      })
+  mounted() {
+    $eventbus.$on("changeLang", (res) => {
+      this.ids = this.$route.params.id
+      localStorage.setItem('bookId', this.ids)
+      this.getdetailbook(this.ids)
+    })
   },
   methods: {
-      toreplace(){
-         this.$router.go(-1)
-       },
-    getdetailbook(id){
-      interfaces.NObookdetail(id).then(res=>{
-        this.listInfo=res
+    toreplace() {
+      this.$router.go(-1)
+    },
+    getdetailbook(id) {
+      interfaces.NObookdetail(id).then(res => {
+        this.listInfo = res
       })
     },
     showPopup() {
       this.show = true;
     },
     closePopup() {
-      if(this.desc==''){
-        return;
+      if (this.desc == '') {
         this.$toast(this.$i18n.t('m.show1'))
-      }else{
-      let id=this.ids
-      let status=3
-      let desc=this.desc
-      let data={id,status,desc}
-      interfaces.jieshou(data).then(res=>{
-      if(res==true){
+        return;
+      } else {
+        let id = this.ids
+        let status = 3
+        let desc = this.desc
+        let data = { id, status, desc }
+        interfaces.jieshou(data).then(res => {
+          this.$toast(res)
+          setTimeout(() => {
+            this.show = false;
+            this.$router.go(-1);
+          });
+        }).catch((err) => {
+          console.log(err);
           this.show = false;
-       }
-      })
+        })
       }
     },
     showRight() {
-      let id=this.ids
-      let status=2
-      let data={id,status}
-      interfaces.jieshou(data).then(res=>{
-          this.show_right = true;
-        setTimeout(()=>{
-         this.show_right = false;
-         this.$router.replace({name:'booking',params:{active:1,num:2}})
-        },1000)
+      let id = this.ids
+      let status = 2
+      let data = { id, status }
+      interfaces.jieshou(data).then(res => {
+        this.show_right = true;
+        setTimeout(() => {
+          this.show_right = false;
+          this.$router.replace({ name: 'booking', params: { active: 1, num: 2 } })
+        }, 1000)
       })
-      
+
     },
-    tohideModel(){
-       this.show = false;
+    tohideModel() {
+      this.show = false;
     }
   }
 };
@@ -168,7 +179,12 @@ export default {
 
 <style scoped>
 .bookingdetail {
-width:100%;margin:0 auto; min-height: 100%;padding-bottom:5.96rem;box-sizing: border-box;position:relative;
+  width: 100%;
+  margin: 0 auto;
+  min-height: 100%;
+  padding-bottom: 5.96rem;
+  box-sizing: border-box;
+  position: relative;
 }
 
 .top {
@@ -196,14 +212,13 @@ width:100%;margin:0 auto; min-height: 100%;padding-bottom:5.96rem;box-sizing: bo
 .detail {
   width: auto;
   height: auto;
-  border-top:1px solid #f1f1f1;
-
+  border-top: 1px solid #f1f1f1;
 }
 .detail ul {
   width: 100%;
   height: auto;
   margin: 0 auto;
-  padding-top:0.88rem;
+  padding-top: 0.88rem;
 }
 .detail ul li {
   padding: 0 0.6rem;
@@ -226,7 +241,9 @@ width:100%;margin:0 auto; min-height: 100%;padding-bottom:5.96rem;box-sizing: bo
   display: inline-block;
   width: 2.5rem;
 }
-.detail ul .detail-li{margin-top:0.8rem;}
+.detail ul .detail-li {
+  margin-top: 0.8rem;
+}
 .detail-button {
   width: 9.59rem;
   height: auto;
@@ -267,16 +284,16 @@ width:100%;margin:0 auto; min-height: 100%;padding-bottom:5.96rem;box-sizing: bo
   margin: 0.2rem auto 0.4rem;
   display: block;
   background: rgb(89, 117, 169);
-  color:#fff;
+  color: #fff;
 }
 
-.box .box-btn[disabled]{
-    width: 90%;
+.box .box-btn[disabled] {
+  width: 90%;
   height: auto;
   margin: 0.2rem auto 0.4rem;
   display: block;
-  color:#333;
-   background-color: #dddddd;
+  color: #333;
+  background-color: #dddddd;
 }
 
 /* .box .van-button .van-button__text {
@@ -301,9 +318,15 @@ width:100%;margin:0 auto; min-height: 100%;padding-bottom:5.96rem;box-sizing: bo
   margin-top: 0.5rem;
 }
 .van-popup {
-    border-radius: 10px;
-    overflow:visible;
+  border-radius: 10px;
+  overflow: visible;
 }
 
-.img_remove{width:0.82rem;height:0.82rem;position: absolute;bottom:-53px;left:47%;}
+.img_remove {
+  width: 0.82rem;
+  height: 0.82rem;
+  position: absolute;
+  bottom: -53px;
+  left: 47%;
+}
 </style>
